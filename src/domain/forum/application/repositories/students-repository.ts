372ec8585bfa84c7c5af2d1837e69ef933c0e6ed
Frugoli <henrique_frugoli@outlook.com>
2024-02-19 20:39:00 -0300

    feat: create and authenticate student

--- src/domain/forum/application/repositories/students-repository.ts
+++ src/domain/forum/application/repositories/students-repository.ts
@@ -1,8 +1,8 @@
-/* eslint-disable semi */
-// eslint-disable-next-line prettier/prettier
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line semi
 import { Student } from '../../enterprise/entities/student';
 
-export abstract class QuestionsRepository {
+export abstract class StudentRepository {
   abstract findByEmail(email: string): Promise<Student | null>
   abstract create(student: Student): Promise<void>
 }
